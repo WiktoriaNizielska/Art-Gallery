97437128f9eee343470d96a1857675a778892b9e
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2025-02-04 15:50:18 +0100

    images styled

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,15 @@ function App() {
 
   return (
     <>
-      <header>Art gallery - Art Institute of Chicago</header>
-      <main>
+      <header className="m-2">Art gallery - Art Institute of Chicago</header>
+      <main className="max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-1 grid grid-cols-5 gap-5 m-2 ">
         {data?.images.map((image) => (
-          <div key={image.id}>
-            <img src={image.imageUrl || undefined} alt={image.title}></img>
+          <div key={image.id} className="max-w-3xs mb-10 ">
+            <img
+              src={image.imageUrl || undefined}
+              alt={image.title}
+              className=" w-full h-full object-cover"
+            ></img>
             <h2>{image.title}</h2>
           </div>
         ))}
