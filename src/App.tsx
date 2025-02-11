d41d989fcc9b4779ef79696c4d1927a93ba40e92
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2025-02-11 15:48:50 +0100

    shadow added to images

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,11 @@ function App() {
       <header className="m-2">Art gallery - Art Institute of Chicago</header>
       <main className="max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-1 grid grid-cols-5 gap-5 m-2 ">
         {data?.images.map((image) => (
-          <div key={image.id} className="max-w-3xs mb-10 ">
+          <div key={image.id} className="max-w-3xs mb-10">
             <img
               src={image.imageUrl || undefined}
               alt={image.title}
-              className=" w-full h-full object-cover"
+              className=" w-full h-full object-cover shadow-lg"
             ></img>
             <h2>{image.title}</h2>
           </div>
