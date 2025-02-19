fab79ade77c145a81b1036fbe8a3181bfbebc2d8
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2025-02-19 09:15:31 +0100

    build added to config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Art-Gallery/"
+  base: "/Art-Gallery/",
+  build: {
+    outDir: "build",
+  },
 })
